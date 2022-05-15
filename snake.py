f0b0d2f495b80ef8232f9e3fcbebefe8f2dc279e
JKodes <adrainford09@gmail.com>
2022-05-14 21:40:24 -0400

    create a game speed so that no matter the computer all can play at same rate

--- snake.py
+++ snake.py
@@ -30,13 +30,36 @@ blue = (0, 102, 204) #apple color
 yellow = (255, 255, 51) #snake color 
 green  = (0, 153, 0) #grass color
 white = (255, 255, 255) #font color
+purple = (106, 13, 173) #random back splash
 
 
 #Fonts
+font = pygame.font.SysFont('Helvetica', 40)
 
 
 #Display Text
 
+#Intro Title OF The Game Name
+game_title_text = font.render('Snake', True, white, purple)
+title_rect = game_title_text.get_rect()
+title_rect.center = (window_width//2, window_height//2)
+
+#Score Display For Left Hand Top Corner
+score_display_text = font.render('Score: ' + str(score), True, white, purple )
+score_rect = score_display_text.get_rect()
+score_rect.topleft = (25, 25)
+
+#Character's for the game
+
+
+
+
+
+
+#Play Again Text Display
+play_again_text = font.render("Play Again Hit Any Key", True, white, purple )
+play_again_text_rect = play_again_text.get_rect()
+play_again_text_rect.center = (window_width//2, window_height//2)
 
 #Main game loop
 playing = True
